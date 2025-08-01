5487967cdd12be52a994c1c391f5316ea67002ad
PinkMikalai <job.m.sashko@gmail.com>
2025-08-01 12:40:24 +0200

    prece que terminne i lfaut corriger

--- public/index.php
+++ public/index.php
@@ -1,22 +1,25 @@
 <?php
 session_start();
 
-$newUser = null; // Initialisation pour éviter erreur si formulaire vide
+$newUser = []; // Initialisation pour éviter erreur si formulaire vide
 $erreurs = [];
 
+if (!isset($_SESSION["users"])) {
+       $_SESSION["users"] = [];
+    }
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom = $_POST["nom"] ?? '';
-    $age = $_POST["age"] ?? '';
-    $city = $_POST["city"] ?? '';
+    $nom = htmlspecialchars($_POST["nom"] )?? '';
+    $age = htmlspecialchars($_POST["age"]) ?? '';
+    $city = htmlspecialchars($_POST["city"])?? '';
 
     if (!empty($nom) && !empty($age) && !empty($city)) {
-        $newUser = [
+        $newUser[] = [
             "nom" => $nom,
             "age" => $age,
             "city" => $city
         ];
-        $_SESSION['user'] = $newUser; // on fait une session user apres avoir envoyee la formulaire par POST
-        var_dump($newUser); // affichage test avec nos cordonnes
     }
     if (empty($nom)){
         $erreurs[] = "ta pas ton nom";
@@ -30,6 +33,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $erreurs[] = "ta pas ta ville";
         var_dump($erreurs);
     }
+    $_SESSION['user'][] = $newUser;
+    $users = $_SESSION['user'];
 }
 ?>
 
@@ -61,6 +66,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <input type="submit" value="Envoyer">
             </div>
         </form>
+        <section>
+            <h2>Ici mon message recuee</h2>
+            <article>
+                <?php 
+                    if(!empty($newUser)) {
+                        foreach ($users as $user) {
+                            echo "
+                            <article>
+                                <h3>$user[nom]</h3>
+                                <span>l'age est: $user[age]</span>
+                                <p>
+                                    $user[city]
+                                </p>
+                            </article>
+                            ";
+                        }
+                        
+                    }else{
+                        echo "
+                            <p>
+                                Nous n'avons pas reçu de messages!
+                            </p>
+                        ";
+                    }
+                ?>
+            </article>
+        </section>
     </main>
     <?php include "../includes/footer.html"; ?>
 </body>
